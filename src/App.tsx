d843ce600df743a7250d4d089f6eda6de377fc4e
dolphin <p3348225655@163.com>
2024-07-18 20:32:03 +0800

    初始化仓库

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./components/dashboard/Dashboard";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+};
+
+export default App;
